6c55aca9f9a32f3aa3e388b662692564dd44920c
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-01 14:02:41 -0600

    Example Feb 1

--- In_class_example_Feb 1/Conversion Example/Program.cs
+++ In_class_example_Feb 1/Conversion Example/Program.cs
@@ -6,7 +6,25 @@ namespace Conversion_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int heresMyInt;
+            //double heresMyDouble;
+
+
+
+            heresMyDouble = 5.5;
+            heresMyInt = 5;
+
+            double result = heresMyDouble + heresMyInt;
+
+            //Console.WriteLine("The result of" + heresMyDouble.ToString("N3") + " + " +
+            //    heresMyInt.ToString + " = " + result.ToString("C5"));
+
+            Console.WriteLine($"The result of {heresMyDouble.ToString("N3")} + {heresMyInt.ToString()} = {result.ToString("C5")}.)");
+
+
+
+      
+
         }
     }
 }
