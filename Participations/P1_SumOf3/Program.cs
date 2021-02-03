e74b3b1124906e5601a37eb2f1d6f831e08bb762
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-03 13:09:57 -0600

    Project 1: Sum of 3

---
+++ Participations/P1_SumOf3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P1_SumOf3
+{
+    class Program
+    {
+        static double ClassLevelVariable = 5;
+        static void Main(string[] args)
+        {
+           const double MAGIC_NUMBER = 7.777; // local variable that is a constant 
+
+            // Can't do this because it is a constant value MAGIC_NUMBER = 8;
+            ClassLevelVariable = 8;
+
+        }
+    }
+}
