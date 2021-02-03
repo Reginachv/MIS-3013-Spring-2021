31533dfc62b740492d9ea89934dbd1dabdae2756
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-03 14:45:07 -0600

    Project 1 Sum of 3's

--- Participations/P1_SumOf3/Program.cs
+++ Participations/P1_SumOf3/Program.cs
@@ -2,15 +2,25 @@
 
 namespace P1_SumOf3
 {
-    class Program
+    
+       class Program
     {
-        static double ClassLevelVariable = 5;
-        static void Main(string[] args)
-        {
-           const double MAGIC_NUMBER = 7.777; // local variable that is a constant 
+       // static double ClassLevelVariable = 5;
+    static int Main(string[] args)
+    {
+        //const double MAGIC_NUMBER = 7.777; // local variable that is a constant 
 
-            // Can't do this because it is a constant value MAGIC_NUMBER = 8;
-            ClassLevelVariable = 8;
+        // Can't do this because it is a constant value MAGIC_NUMBER = 8;
+        //ClassLevelVariable = 8;
+
+        
+
+           
+
+
+
+
+
 
         }
     }
---
+++ Sum of 3 Project 1/Project1_Sumof3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project1_Sumof3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static double sum(double n1, double n2, double n3)
+            in total;
+            total = n1 + n2 + n3;
+            return total;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate sum of 3 numbers");
+            Console.WriteLine("Enter number 1");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            double n3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Calculate sum of 3 numbers is: { 0:F3}", sum(n1, n2, n3));
+
+        }
+
+
+
+
+
+
+
+    }
+}
+}
