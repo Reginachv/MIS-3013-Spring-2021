c4ec07826a2f78d54d7a38c847bb93d0c3b64531
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-10 13:10:05 -0600

    Finished Class Example Fizz Buzz

--- In Class -Fizz Buzz/Class_Example_Feb10/Program.cs
+++ In Class -Fizz Buzz/Class_Example_Feb10/Program.cs
@@ -6,7 +6,34 @@ namespace Class_Example_Feb10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string DEVELOPER_INFO = "Regina Chavez";
+            Random rand = new Random();
+            int randomNumberBetween1and100 = rand.Next(1, 101);
+
+            if (randomNumberBetween1and100 % 3 == 0)
+
+            {
+                Console.WriteLine("Fizz");
+
+            }
+
+            else if (randomNumberBetween1and100 % 5 == 0)
+
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else if (randomNumberBetween1and100 % 3 == 0 &
+                randomNumberBetween1and100 % 5 == 0)
+
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine($"{randomNumberBetween1and100}");
+            }
+             Console.WriteLine($"Developed by {DEVELOPER_INFO}");
         }
     }
 }
