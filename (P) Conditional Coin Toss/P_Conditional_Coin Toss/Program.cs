8ee1960c5e1545c20e0cafef77edb86bcd23a195
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-11 22:50:30 -0600

    Finished Project Coin Toss

--- (P) Conditional Coin Toss/P_Conditional_Coin Toss/Program.cs
+++ (P) Conditional Coin Toss/P_Conditional_Coin Toss/Program.cs
@@ -10,39 +10,43 @@ namespace P_Conditional_Coin_Toss
         {
             const string DEVELOPER_INFO = "Regina Chavez";
 
-            Console.WriteLine("Guess heads by entering 1 or tails by entering 2 for a coin toss");
-            int answer = Convert.ToInt32(Console.ReadLine());
+            int answer = 0;
+
+            while(answer != 1 && answer != 2)
+            {
+                Console.WriteLine("Guess heads by entering 1 or tails by entering 2 for a coin toss");
+                answer = Convert.ToInt32(Console.ReadLine());
+            }
 
             if (answer == 1)
             {
                 Console.WriteLine("You guessed heads");
             }
+            else {
+                Console.WriteLine("You guessed tails");
+            }
+
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 3);
+            Console.WriteLine($"{randomNbr}");
+
+            if (randomNbr == 1)
+            {
+                Console.WriteLine("The coin landed on Heads");
+            }
             else
             {
-                Console.WriteLine("You guessed tails");
-
-                Random rand = new Random();
-                int randomNbr = rand.Next(1, 3);
-                Console.WriteLine($"{randomNbr}");
-
-                if (randomNbr == 1)
-                {
-                Console.WriteLine("The coin landed on Heads");
-                }
-            else
-                {
                 Console.WriteLine("The coin landed on Tails");
-                }
-            if (randomNbr = answer)
-                {
-               Console.WriteLine("Your guess was correct");
-                }
-            else
-                {
-                Console.WriteLine("Your guess was incorrect, sorry :(");
-                }
-                Console.WriteLine($"Developed by {DEVELOPER_INFO}");
             }
+            if (randomNbr == answer)
+            {
+                Console.WriteLine("Your guess was correct");
+            }
+            else
+            {
+                Console.WriteLine("Your guess was incorrect, sorry :(");
+            }
+            Console.WriteLine($"Developed by {DEVELOPER_INFO}");
         }
     }
 }
---
+++ Cointoss Attempt 2/CoinToss_Attempt2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoinToss_Attempt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
