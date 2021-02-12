4ca895de06fce019b629f785e60a9ae88c5f062e
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-11 21:39:04 -0600

    Progressed with Coin toss Project

--- (P) Conditional Coin Toss/P_Conditional_Coin Toss/Program.cs
+++ (P) Conditional Coin Toss/P_Conditional_Coin Toss/Program.cs
@@ -4,9 +4,48 @@ namespace P_Conditional_Coin_Toss
 {
     class Program
     {
+        public static bool Head { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string DEVELOPER_INFO = "Regina Chavez";
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 3);
+            Console.WriteLine($"{randomNbr}");
+
+            //1 = heads
+            //2 = tails
+
+            Console.WriteLine("Guess heads by entering 1 or tails by entering 2 for a coin toss");
+            string answer = Console.ReadLine();
+            
+
+            if (answer == 1)
+            {
+                Console.WriteLine("You guessed heads");
+            }
+            else
+            {
+                Console.WriteLine("You guessed tails");
+
+            if (randomNbr == 1)
+                {
+                Console.WriteLine("The coin landed on Heads");
+                }
+            else
+                {
+                Console.WriteLine("The coin landed on Tails");
+                }
+            if (randomNbr = answer)
+                {
+               Console.WriteLine("Your guess was correct");
+                }
+            else
+                {
+                Console.WriteLine("Your guess was incorrect, sorry :(");
+                }
+                Console.WriteLine($"Developed by {DEVELOPER_INFO}");
+            }
         }
     }
 }
