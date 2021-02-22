31bb2c04e30ca0e860419e2e1a77ee9365a51258
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-22 13:50:15 -0600

    Started Random Guess

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            Console.WriteLine("Knock Knock. Who's there? >>");
+            answer = Console.ReadLine();
+
+            while (answer != "banana")
+
+            {
+                Console.WriteLine("Knock knock. Who's there? >>");
+                answer = Console.ReadLine();
+                {
+                    int i = 3;
+                    while (i <= 1000)
+                    {
+                        Console.WriteLine(1);
+                        i += 3;
+                    }
+                    {
+                        int favoriteNumber;
+                        Console.WriteLine("What is your favorite number");
+                        string answer = Console.ReadLine();
+                    
+                        while (int.TryParse(answer))
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RandomGuess/RandomGuess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1, 11);
+
+            Console.WriteLine($"randomNbr");
+
+            Console.WriteLine("\nGuess a number between 1 and 10");
+            Console.ReadLine();
+        }
+    }
+}
