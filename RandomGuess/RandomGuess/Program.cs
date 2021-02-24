47f48f24fe7c8a5806f735a8d6be44320c171441
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-24 11:30:25 -0600

    Progressed on Random Guess

--- RandomGuess/RandomGuess/Program.cs
+++ RandomGuess/RandomGuess/Program.cs
@@ -8,11 +8,32 @@ namespace RandomGuess
         {
             Random rand = new Random();
             int randomNbr = rand.Next(1, 11);
+            int guess = 0;
 
-            Console.WriteLine($"randomNbr");
+            Console.WriteLine("\nI am thinking of a number between 1 and 10, can you guess what it is?");
+            
 
-            Console.WriteLine("\nGuess a number between 1 and 10");
-            Console.ReadLine();
+            do
+            {
+                Int32.TryParse(Console.ReadLine(), out guess);
+                if (guess > 10 || guess < 1)
+                    Console.WriteLine("Please choose a number between 1 and 10");
+            } while (guess > 10 || guess < 1);
+
+            for (int i = 0; i < guess; i++)
+                Console.WriteLine("Please choose a number between 1 and 10");
+                Console.ReadLine();   
+
+            while (guess != randomNbr)
+                { 
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            while (guess == randomNbr)
+            {
+                Console.WriteLine("Great Job! Your guess was correct");
+                Console.ReadLine();
+            }
         }
     }
 }
+    
