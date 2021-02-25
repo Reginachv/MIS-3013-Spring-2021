7f4d30baf054740995285b0a1307e71c2e78386b
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-02-24 21:33:41 -0600

    Progressed random guess

--- RandomGuess/RandomGuess/Program.cs
+++ RandomGuess/RandomGuess/Program.cs
@@ -4,40 +4,36 @@ namespace RandomGuess
 {
     class Program
     {
+        private const string Arg1 = "}";
+
         static void Main(string[] args)
         {
+           
             Random rand = new Random();
             int randomNbr = rand.Next(1, 11);
-            int guess = 0;
 
             Console.WriteLine("\nI am thinking of a number between 1 and 10, can you guess what it is?");
-            Console.ReadLine();
+            int guess = Convert.ToInt32(Console.ReadLine());
 
-            /*
-                do
+            while (guess > randomNbr)
             {
-                Int32.TryParse(Console.ReadLine(), out guess);
-                if (guess > 10 || guess < 1)
-                    Console.WriteLine("Please choose a number between 1 and 10");
-            } while (guess > 10 || guess < 1);
-
-            for (int i = 0; i < guess; i++)
-                Console.WriteLine("Please choose a number between 1 and 10");
-                Console.ReadLine();  **/
-           
-            while (guess != randomNbr)
-            {
-                Console.WriteLine("Sorry, Please Try Again");
-                Console.ReadLine();
-
+                Console.WriteLine($"No, that too high. Guess lower!");
                 guess = Convert.ToInt32(Console.ReadLine());
             }
-            while (guess == randomNbr)
+            while (guess < randomNbr)
             {
-                Console.WriteLine("Great Job! Your guess was correct");
-                Console.ReadLine();
+                Console.WriteLine($"No, thats too low. Guess higher!");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            while (guess != randomNbr)
+            {
+                Console.WriteLine($"Sorry, Please Try Again");
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+                    Console.WriteLine($"Great Job! Your guess was correct its {randomNbr}");
+                }
             }
         }
-    }
-}
+
+
     
