83c76981874d70272f4bb47ea563e25a53971c40
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-01 13:26:18 -0600

    Progressed participation

---
+++ (P) String Maipulation/(P) String Maipulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _P__String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = ("Programming today is a race software engineers striving engineers striving to build bigger and better idiot proof programs, and the univers trying to build bigger and better idioys. So far, the universe is winning.");
+            Console.WriteLine(newMssage);
+
+            Console.WriteLine("What word are you trying to find and replace in the statement above?");
+            Console.ReadLine();
+
+            Console.WriteLine("With what word would you like to replace it with?");
+            Console.ReadLine();
+        }
+    }
+}
+
+     
