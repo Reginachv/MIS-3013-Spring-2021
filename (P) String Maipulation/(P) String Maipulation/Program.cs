f10c75b86c6e060542a704c05a80450b848bd3c1
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-03 10:40:46 -0600

    progressed (P) String manipulation

--- (P) String Maipulation/(P) String Maipulation/Program.cs
+++ (P) String Maipulation/(P) String Maipulation/Program.cs
@@ -6,8 +6,10 @@ namespace _P__String_Manipulation
     {
         static void Main(string[] args)
         {
-            string message = ("Programming today is a race software engineers striving engineers striving to build bigger and better idiot proof programs, and the univers trying to build bigger and better idioys. So far, the universe is winning.");
-            Console.WriteLine(newMssage);
+            string message = ("Programming today is a race software engineers striving engineers striving to build bigger and better idiot proof programs, and the univers trying to build bigger and better idiots. So far, the universe is winning.");
+            string newMessage = 
+
+            Console.WriteLine(newMessage);
 
             Console.WriteLine("What word are you trying to find and replace in the statement above?");
             Console.ReadLine();
