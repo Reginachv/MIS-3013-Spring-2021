26a35b353507a164e8a3facc95b99c8f76148af1
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-15 13:31:43 -0500

    Part 1

---
+++ Midterm 1 Review/Homework 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine();
+            Environment.Exit(-1);
+            Console.ReadLine();
+
+            if()
+            {
+
+            }
+            else if
+                {
+
+            }
+            else
+            {
+
+            }
+            }*/
+
+        }
+    }
+}
