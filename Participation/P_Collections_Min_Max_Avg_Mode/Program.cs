712f5a6582d887fee4c0874683db1423c8335a26
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-17 13:54:46 -0500

    Participation Collections

---
+++ Participation/P_Collections_Min_Max_Avg_Mode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P_Collections_Min_Max_Avg_Mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> AllExamGrades = new List<string>();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please enter all your exam grades (e.g. 98, 72, 32");
+                answer = Console.ReadLine();
+
+                // string[] gradesSplit = allGrades.Split(',');
+
+            } while ( );
+
+        }
+    }
+}
