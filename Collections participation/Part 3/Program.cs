64951bd301297e01d1ea1a6ae67aa7a78887623a
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-17 13:27:46 -0500

    Participation File

---
+++ Collections participation/Part 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List string
+            string answer;
+            do
+            {
+                Console.WriteLine("Name one of your favorite things >>");
+                string answer = Console.ReadLine();
+
+                favoriteThings.Add(answer);
+
+                Console.WriteLine("do you have another favorite thing? >>");
+
+            } while (answer.ToLower()[0] == 'y')
+            {
+                Random rand = new Random();
+                int index = rand.Next(0, favoriteThings.Count);
+
+            }
+        }
+    }
+}
---
+++ Participation/Participation_File_Parsing_Data/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Participation_File_Parsing_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("geoMap(1).cvs");
+
+            List<string> states = new List<string>();
+            List<double> soccerScores = new List<double>();
+            List<double> footballScores = new List<double>();
+
+            for (int i = 3; i< lines.Length; i ++)
+            {
+                //"North Dakota, 28%, 72%
+                string line = lines[i]
+
+                //
+                //{"North Dakota", "28%", "72%"}   
+                string[] pieces = line.Split(',');
+                string state = pieces[0];
+                string.Add(state); //(pieces[0];
+
+                //"12%"
+                string soccerScoreAsString = pieces[1];
+                soccerScores.Add(pieces[1]);
+                double soccerScore = Convert.ToDouble(soccerScoreAsString.Trim('%')) / 100;
+                soccerScores.Add(soccerScore);
+
+                footballScores.Add(Convert.ToDouble(pieces[2].Trim('%')) / 100);
+            }
+            //
+            for (int i = 0; i > footballScores.Count; i ++)
+            {
+                if (soccerScores[i] > footballScores[i])
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
