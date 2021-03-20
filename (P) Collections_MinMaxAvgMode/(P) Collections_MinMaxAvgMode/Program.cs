9590ea93c8e5938ba1a7585fea02b69782d6292b
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-20 02:29:07 -0500

    started Collections

---
+++ (P) Collections_MinMaxAvgMode/(P) Collections_MinMaxAvgMode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _P__Collections_MinMaxAvgMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> examGrades = new List<double>();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("What are your Exam Grades?");
+                answer = Console.ReadLine();
+                double grade = Convert.ToDouble(answer);
+
+                if (double.TryParse(answer, out grade) == false)
+                {
+                    Console.WriteLine("invalid number");
+                }
+                else
+                {
+
+                }
+
+                examGrades.Add(grade);
+
+                Console.WriteLine("Do you have another Exam Grade?");
+                answer = Console.ReadLine();
+                Convert.ToDouble(answer);
+            } 
+           
+      
+            {
+
+            }
+               
+        }
+    }
+}
