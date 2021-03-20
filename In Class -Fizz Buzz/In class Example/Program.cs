05281cde7ee368eac4896d7ebd81d75a70dee00b
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-20 01:32:41 -0500

    Moved to combine all in class examples

---
+++ In Class -Fizz Buzz/In class Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace In_class_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // static double ClassLevelVariable = 5;
+            static int Main(string[] args)
+            {
+                //const double MAGIC_NUMBER = 7.777; // local variable that is a constant 
+
+                // Can't do this because it is a constant value MAGIC_NUMBER = 8;
+                //ClassLevelVariable = 8;
+            }
+        }
+}
