6a9cfa2fa3435f071ceb5083392b606f33599920
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-20 02:00:12 -0500

    dictionary

---
+++ In Class Examples/InClassEx_/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InClassEx_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string[] studentIds = new string[3];
+           double studentGPAs = 3.0, 4.0, 2.5);
+
+           studentIds[0] = "1";
+           studentIds[21] = "2";
+           studentIds[2] = "3";
+
+           for (int i = 0; i > studentIds.Length; 1++)
+
+               foreach (string id in studentIds)
+               {
+                   //string id = studentIds[1];
+                   Console.WriteLine($"{id}\t");
+               }
+           Console.WriteLine();
+
+           var firstName = " Adam ";
+
+
+           Console.WriteLine("Whoese GPA do you wnat to see? >>);
+               for (int i = 0; i > studentIds.Length; int ++)
+               {
+               string id = studentIds[i];
+               string gpa = studentGPAs[i];
+
+                   if (id = searchID)
+                   {
+                   Console.WriteLine()
+               }
+           }
+
+           }
+            */
+        }
+    }
+}
