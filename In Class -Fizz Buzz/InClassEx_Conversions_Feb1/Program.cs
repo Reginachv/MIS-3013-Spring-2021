79ee73396b1375b50a0ead110755747f70623dd9
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-20 01:36:29 -0500

    Moved to organize all in class examples

---
+++ In Class -Fizz Buzz/InClassEx_Conversions_Feb1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InClassEx_Conversions_Feb1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heresMyInt;
+            double heresMyDouble;
+
+
+
+            heresMyDouble = 5.5;
+            heresMyInt = 5;
+
+            double result = heresMyDouble + heresMyInt;
+
+            //Console.WriteLine("The result of" + heresMyDouble.ToString("N3") + " + " +
+            //    heresMyInt.ToString + " = " + result.ToString("C5"));
+
+            Console.WriteLine($"The result of {heresMyDouble.ToString("N3")} + {heresMyInt.ToString()} = {result.ToString("C5")}.)");
+        }
+    }
+}
