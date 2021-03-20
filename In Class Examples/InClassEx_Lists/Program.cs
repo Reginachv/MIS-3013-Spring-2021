e1744ff1fc33939d81eae7be4cb2e8b84df39f20
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-20 02:01:56 -0500

    lists example

---
+++ In Class Examples/InClassEx_Lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InClassEx_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List string
+            /*string answer;
+            do
+            {
+                Console.WriteLine("Name one of your favorite things >>");
+                string answer = Console.ReadLine();
+
+                favoriteThings.Add(answer);
+
+                Console.WriteLine("do you have another favorite thing? >>");
+
+            } while (answer.ToLower()[0] == 'y')
+            {
+                Random rand = new Random();
+                int index = rand.Next(0, favoriteThings.Count);*/
+            }
+    }
+}
