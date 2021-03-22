642141b69b834982af9893c537f98c7372a594f3
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-22 13:49:43 -0500

    collections

--- (P) Collections_MinMaxAvgMode/(P) Collections_MinMaxAvgMode/Program.cs
+++ (P) Collections_MinMaxAvgMode/(P) Collections_MinMaxAvgMode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace _P__Collections_MinMaxAvgMode
 {
@@ -8,41 +9,52 @@ namespace _P__Collections_MinMaxAvgMode
         static void Main(string[] args)
         {
             List<double> examGrades = new List<double>();
+            Dictionary<double, int> exams = new Dictionary<double, int>();
             Console.WriteLine("How many exam grades do you have?");
             int howManyExams = Convert.ToInt32(Console.ReadLine());
+            string answer;
+
+            /*  for (int i = 0; i < howManyExams; i++)
+              {
+                  Console.WriteLine("Name the Exam name");
+                  String examName = new string(Console.ReadLine());
+              }*/
 
             for (int i = 0; i < howManyExams; i++)
             {
-                Console.WriteLine("Name the Exam name");
-                String examName = new string(Console.ReadLine());
-            }
+            
+                    Console.WriteLine("What is your exam grade? (e.g. 92)");
+                    answer = Console.ReadLine();
 
-            for (int i = 0; i < howManyExams; i++)
+                    double grade = Convert.ToDouble(answer);
+                 
+               }
+
+            answer = Console.ReadLine();
+            double min = examGrades[0];
+
+            foreach (double grade in examGrades)
             {
-                Console.WriteLine("What is your exam grade?");
-                string answer = Console.ReadLine();
-                double grade;
-
-                if (double.TryParse(answer, out grade) == false)
+                if (grade < min)
                 {
-                    Console.WriteLine("invalid exam grade");
-                }         
+                    min = grade;
+                }
+                if (exams.ContainsKey(grade) == true)
+                {
+                    exams[grade] = exams[grade] + 1;
+                }
                 else
                 {
-                   
+                    exams.Add(grade, 1);
                 }
+            }
 
-                Console.WriteLine();
-                
-                //min;max;avg;mode
+            Console.WriteLine($"The minimimum exam grade is {min.ToString("N2")}");
 
-
-
-
+                }
             }
         }
-    }
-}
+    
 
  
 
