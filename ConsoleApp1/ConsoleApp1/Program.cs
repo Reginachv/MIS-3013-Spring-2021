be8a0e9e8bc19acee57a8773812942dc3c41276e
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-31 13:31:57 -0500

    Participation of function started

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,14 +8,14 @@ namespace ConsoleApp1
         {
             double sum = Add(3, 4); 
         }
-        // Allows to pop up when inputting code.
+        // Another way of commenting it allows comments to pop up when inputting code.
         /// <summary>
         /// sums up two v
         /// </summary>
         /// <param name="val1">The left hand operand of the equations</param>
         /// <param name="val2">The right hand operand of the equations</param>
         /// <returns>The sum of the two numbers</returns>
-        static double Add(double val1, double val2)
+        static double Add(double val1, double val2) // val1 and val2 are parameters (param)
         {
             double number = val1 + val2;
             return number;
