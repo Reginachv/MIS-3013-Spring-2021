a17521f46bd61c867bb67a54150fb78e0e61e625
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-03-31 13:15:21 -0500

    In class example Functions

---
+++ In Class Examples/InClassEx_Functions_March31/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InClassEx_Functions_March31
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            List<double> numbers = new List<double>();
+            string answer;
+
+            do
+            {
+                double gpa = ValidateDoubleInput("Please enter a GPA >>");
+                gpas.Add(gpa)
+                Console.WriteLine("Do you want to enter more numbers? yes or no >> ");
+                answer = Console.ReadLine();
+            } while (answer.ToLower()[0] == 'y'); // only check if there is a "y" not necessarily yes or no
+        }
+
+        static double ValidateDoubleInput(string message)
+        {
+            double value;
+            string answer;
+            //Console.WriteLine( "Do you want to go agian yes or no? >>")
+            // while (answer != "yes" && answer != "no")
+            {
+                Console.WriteLine("Sorry, answer yes or no only");
+                Console.WriteLine("Do you want to go again? yes or no");
+                answer = Console.ReadLine().ToLower();
+            }
+            if (answer == "yes")
+
+            do
+            {
+                Console.WriteLine(message);
+                answer = Console.ReadLine();
+
+                //bool isDouble = doble.TryParse(answer, out value):
+            } while (double.TryParse((answer, out value) == false)
+
+            /*List<double> itemPrices = new List<double>();
+            string answer;
+
+            do
+            {
+                double gpa = ValidateDoubleInput("Please enter a GPA >>");
+                gpas.Add(gpa);
+
+                Console.WriteLine("Do you want to enter more numbers? yes or no >> ");
+                answer = Console.ReadLine();
+            } while (answer.ToLower()[0] == 'y'); */
+        }
+    }
+    }
+}
