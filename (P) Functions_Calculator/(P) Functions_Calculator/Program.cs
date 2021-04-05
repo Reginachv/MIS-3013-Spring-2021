f09be84ce965f78dcf9ea793fbb99b178711e592
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-04-05 13:49:52 -0500

    Participation started for Functions

---
+++ (P) Functions_Calculator/(P) Functions_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _P__Functions_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,21 +6,49 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            double sum = Add(3, 4); 
-        }
-        // Another way of commenting it allows comments to pop up when inputting code.
-        /// <summary>
-        /// sums up two v
-        /// </summary>
-        /// <param name="val1">The left hand operand of the equations</param>
-        /// <param name="val2">The right hand operand of the equations</param>
-        /// <returns>The sum of the two numbers</returns>
-        static double Add(double val1, double val2) // val1 and val2 are parameters (param)
-        {
-            double number = val1 + val2;
-            return number;
+          /*  // 3 nd 4 are arguements
 
-            //return val1 + val2
+            double sum = Add(3, 4);
+            double difference = Subtract(3, 4);
+            double multiplication = Multiply(3, 4);
+            double Division = Divide(3, 4); */
+
+        } 
+            // Another way of commenting it allows comments to pop up when inputting code.
+            /// <summary>
+            /// sums up two v
+            /// </summary>
+            /// <param name="val1">The left hand operand of the equations</param>
+            /// <param name="val2">The right hand operand of the equations</param>
+            /// <returns>The sum of the two numbers</returns>
+            static double Add(double val1, double val2) // val1 and val2 are parameters (param)
+            {
+                // double number = val1 + val2;
+                // return number;
+
+                return val1 + val2;
+            }
+            static double Subtract(double val1, double val2) // val1 and val2 are parameters (param)
+            {
+                return val1 - val2;
+            }
+            static double Multiply(double val1, double val2) // val1 and val2 are parameters (param)
+            {
+                return val1 * val2;
+            }
+            static double Divide(double val1, double val2) // val1 and val2 are parameters (param)
+            {
+                // double number = val1 + val2;
+                // return number;
+
+                return val1 / val2;
+            }
+            /*static string (string name, string className, string date) // val1 and val2 are parameters (param)
+            {
+
+            } */
         }
     }
-}
+
+
+
