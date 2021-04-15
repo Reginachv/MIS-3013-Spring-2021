7cdb116dff134d0ac89a530b61be1ecc20ab2216
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-04-14 21:39:43 -0500

    started participation Toy

---
+++ (P) Classes_Toy/(P) Classes_Toy/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _P__Classes_Toy
+{
+    class Program
+    {
+        public class Toy
+        {
+            public string Manufacturer;
+            public string Name;
+            public double Price;
+            private string Notes;
+
+            public Toy()
+            {
+
+            }
+            public String GetAisle()
+            {
+
+            }
+
+        }
+    }
+}
+
---
+++ Functions Calculator/2nd way/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2nd_way
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          /*  //introduce more variables to store result for the end calculation if user decides to use result in next 
+            
+            do
+            {
+
+            } while (answer.ToLower() == "yes");
+
+        }
+
+        static void DeveloperInformation(string DeveloperName, string CourseName, string Date)
+        {
+            Console.WriteLine($"{DeveloperName} {CourseName} {Date}"); */
+        }
+
+    }
+}
