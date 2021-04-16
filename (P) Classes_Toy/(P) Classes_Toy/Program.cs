92a2c773a05a72e5b582bd78eca167b919bb722d
Regina Chavez <Regina.Chavez-1@ou.edu>
2021-04-15 23:12:13 -0500

    Finished Participation Classes

--- (P) Classes_Toy/(P) Classes_Toy/Program.cs
+++ (P) Classes_Toy/(P) Classes_Toy/Program.cs
@@ -6,20 +6,93 @@ namespace _P__Classes_Toy
     {
         public class Toy
         {
-            public string Manufacturer;
-            public string Name;
-            public double Price;
-            private string Notes;
+            public string Manufacturer { get; set; }
+            public string Name { get; set; }
+            public double Price { get; set; }
+            private string Notes { get; set; }
+            
 
             public Toy()
             {
-
+                Manufacturer = " "; 
+                Name = " "; 
+                Price = 0; 
+                Notes = " ";
+               //Include Notes : "Available in  colors: Ex. Red, Blue, Black, Gray";
             }
-            public String GetAisle()
+            public Toy(string manufacturer, string name, double price, string notes )
             {
+                this.Manufacturer = manufacturer;
+                this.Name = name;
+                this.Price = price;
+                this.Notes = notes;
+            }
+            
+             public String GetAisle()
+              {
+                
+                string Manu;
+               
+                Manu = Manufacturer.Substring(0, 1).ToUpper();
+                Random rand = new Random();
+                     int randomNumber= rand.Next(1, 25);
 
+                return "The " + Manu + randomNumber;
+              }  
+            public void AllToy() 
+            {
+                Console.WriteLine("\n Manufacturer: " + Manufacturer + "\n Toy Name: " + Name + "\n Price: $" + Price + "\n Notes: " + Notes); // can take out "Notes:" for cleaner look
+            } 
+            static void Main(string[] args) //Main Method
+            {
+                
+
+                Toy[] toy = new Toy[3]; //Collection holds all information for each toy to display to user 
+                toy[0] = new Toy();
+                toy[0].setManufacturer("Nintendo");
+                toy[0].setName("Nintendo Switch");
+                toy[0].setPrice(269.99);
+                toy[0].setNotes("Available in colors: red, black, and gray");
+
+                toy[1] = new Toy();
+                toy[1].setManufacturer("MGA Entertainment");
+                toy[1].setName("Bratz Dolls");
+                toy[1].setPrice(29.54);
+                toy[1].setNotes("Available dolls are: Yasmin, Cloe, Sasha, Jade");
+
+                toy[2] = new Toy();
+                toy[2].setManufacturer("US General");
+                toy[2].setName("Workbench");
+                toy[2].setPrice(19.99);
+                toy[2].setNotes("Kid's can set up and play with this toy workbench that has realistic drill press action");
+               
+                Console.WriteLine("Toy's available:");
+                for (int i = 0; i < toy.Length; i ++)
+                {
+                    toy[i].AllToy();
+                }
+                
             }
 
+            private void setNotes(string Notes) //suggested by question mark => in order to set information 
+            {
+                this.Notes = Notes;
+            }
+
+            private void setPrice(double Price)
+            {
+                this.Price = Price;
+            }
+
+            public void setName(string Name)
+            {
+                this.Name = Name;
+            }
+
+            private void setManufacturer(string Manufacturer)
+            {
+                this.Manufacturer = Manufacturer;
+            }
         }
     }
 }
